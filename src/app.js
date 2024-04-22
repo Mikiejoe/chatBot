a4237068b16c510cc942f033c107575a3bd88347
Mikiejoe <omoshjoe02@gmail.com>
2024-04-22 18:43:05 +0300

    add vercel.json

--- src/app.js
+++ src/app.js
@@ -10,7 +10,9 @@ app.options("*",cors())
 
 app.use("/",chatRouter)
 
-app.listen(5000,"0.0.0.0",()=>{
-    console.log("server running on port 5000")
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log("server running on port "+PORT)
 })
 
